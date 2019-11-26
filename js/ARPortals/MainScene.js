b4c41fbca259f051b5ad89e43ca0951d684834b1
Featherweight <vais.emma@gmail.com>
2019-11-26 18:33:18 -0500

    committing before pod install

--- js/ARPortals/MainScene.js
+++ js/ARPortals/MainScene.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-import React, { Component } from 'react';
-import axios from 'axios';
-import { StyleSheet } from 'react-native';
-import { images } from '../../js/res/images';
+import React, { Component } from "react";
+import axios from "axios";
+import { StyleSheet } from "react-native";
+import { images } from "../../js/res/images";
 
 import {
   ViroSceneNavigator,
@@ -19,31 +19,31 @@ import {
   ViroARImageMarker,
   ViroBox,
   ViroARTrackingTargets,
-} from 'react-viro';
+} from "react-viro";
 
-import ThreeDObject from '../../ARScenes/Portals/ThreeDObject';
+import ThreeDObject from "../../ARScenes/Portals/ThreeDObject";
 
 class MainScene extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      background: 'party',
+      background: "party",
       elements: [
         {
-          name: 'fox',
-          type: 'OBJ',
+          name: "fox",
+          type: "OBJ",
           position: [2, 2, -3],
           scale: [0.01, 0.01, 0.01],
         },
         {
-          name: 'fox',
-          type: 'OBJ',
+          name: "fox",
+          type: "OBJ",
           position: [1, 1.5, -5],
           scale: [0.01, 0.01, 0.01],
         },
         {
-          name: 'fox',
-          type: 'OBJ',
+          name: "fox",
+          type: "OBJ",
           position: [-1, 1, -4],
           scale: [0.01, 0.01, 0.01],
         },
@@ -55,27 +55,27 @@ class MainScene extends Component {
 
   componentDidMount() {
     // let templateId = 2
-    console.log(this.props, 'props in main scene');
+    console.log(this.props, "props in main scene");
     let templateId = this.props.templateId;
     if (templateId === 1) {
       this.setState({
-        background: 'party',
+        background: "party",
         elements: [
           {
-            name: 'fox',
-            type: 'OBJ',
+            name: "fox",
+            type: "OBJ",
             position: [2, 2, -3],
             scale: [0.01, 0.01, 0.01],
           },
           {
-            name: 'fox',
-            type: 'OBJ',
+            name: "fox",
+            type: "OBJ",
             position: [1, 1.5, -5],
             scale: [0.01, 0.01, 0.01],
           },
           {
-            name: 'fox',
-            type: 'OBJ',
+            name: "fox",
+            type: "OBJ",
             position: [-1, 1, -4],
             scale: [0.01, 0.01, 0.01],
           },
@@ -85,23 +85,23 @@ class MainScene extends Component {
       });
     } else {
       this.setState({
-        background: 'beach',
+        background: "beach",
         elements: [
           {
-            name: 'fox',
-            type: 'OBJ',
+            name: "fox",
+            type: "OBJ",
             position: [2, 2, -3],
             scale: [0.01, 0.01, 0.01],
           },
           {
-            name: 'fox',
-            type: 'OBJ',
+            name: "fox",
+            type: "OBJ",
             position: [1, 1.5, -5],
             scale: [0.01, 0.01, 0.01],
           },
           {
-            name: 'fox',
-            type: 'OBJ',
+            name: "fox",
+            type: "OBJ",
             position: [-1, 1, -4],
             scale: [0.01, 0.01, 0.01],
           },
@@ -113,26 +113,36 @@ class MainScene extends Component {
   }
 
   render() {
-    let TagViro360 = this.state.viro360Type || Viro360Video
-      return (
-        <ViroARScene>
-        <ViroAmbientLight color="#ffffff" intensity={200}/>
-          <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={() => {}}>
-            <ViroPortal position={[0, 0, -1]} scale={[0.1, 0.1, 0.1]}>
-              <Viro3DObject
-                source={require('../ARPortals/portal_res/portal_ship/portal_ship.vrx')}
-                resources={[require('../ARPortals/portal_res/portal_ship/portal_ship_diffuse.png'),
-                            require('../ARPortals/portal_res/portal_ship/portal_ship_normal.png'),
-                            require('../ARPortals/portal_res/portal_ship/portal_ship_specular.png')]}
-                type="VRX" />
-            </ViroPortal>
-            <TagViro360 source={{uri: images.background[this.state.background].uri}} loop = {this.state.loop} />
-            {this.state.elements.map((element, index) =>
-            <ThreeDObject key={index} element = {element} />)
-            }
-          </ViroPortalScene>
-        </ViroARScene>);
-    }
+    let TagViro360 = this.state.viro360Type || Viro360Video;
+    return (
+      <ViroARScene>
+        <ViroAmbientLight color="#ffffff" intensity={200} />
+        <ViroPortalScene
+          passable={true}
+          dragType="FixedDistance"
+          onDrag={() => {}}
+        >
+          <ViroPortal position={[0, 0, -1]} scale={[0.1, 0.1, 0.1]}>
+            <Viro3DObject
+              source={require("../ARPortals/portal_res/portal_ship/portal_ship.vrx")}
+              resources={[
+                require("../ARPortals/portal_res/portal_ship/portal_ship_diffuse.png"),
+                require("../ARPortals/portal_res/portal_ship/portal_ship_normal.png"),
+                require("../ARPortals/portal_res/portal_ship/portal_ship_specular.png"),
+              ]}
+              type="VRX"
+            />
+          </ViroPortal>
+          <TagViro360
+            source={{ uri: images.background[this.state.background].uri }}
+            loop={this.state.loop}
+          />
+          {this.state.elements.map((element, index) => (
+            <ThreeDObject key={index} element={element} />
+          ))}
+        </ViroPortalScene>
+      </ViroARScene>
+    );
   }
 }
 
--- js/screens/viewerDashboard.js
+++ js/screens/viewerDashboard.js
@@ -1,43 +1,73 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import { withNavigation } from "react-navigation";
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { withNavigation } from 'react-navigation';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+const dummyPortals = [
+  {
+    name: 'PartyEvent',
+    backgroundId: 2,
+    type: 'custom',
+    imageURL:
+      'https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/res/portal.png',
   },
-});
+  {
+    name: 'BeachVacation',
+    backgroundId: 1,
+    type: 'custom',
+    imageURL:
+      'https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/res/portal.png',
+  },
+];
 
-export default class ViewerDashboard extends Component {
+// creating a row class to instantiate a row from
+export default class Table extends Component {
+  renderRow(portal) {
+    return (
+      <View style={{ flex: 1, alignSelf: 'stretch', maxHeight: 50 }}>
+        <Text>{portal.name}</Text>
+      </View>
+    );
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <Text>Some buttons</Text>
-          <View>
-            <Button
-              title="Portal 1"
-              onPress={() => {
-                this.props.navigation.navigate("ViroApp", {
-                  templateId: 1,
-                });
-              }}
-            />
-            <Button
-              title="Portal 2"
-              onPress={() => {
-                this.props.navigation.navigate("ViroApp", {
-                  templateId: 2,
-                });
-              }}
-            />
-          </View>
-        </View>
+      <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
+        {dummyPortals.map(portal => {
+          return this.renderRow(portal);
+        })}
       </View>
     );
   }
 }
+
+// export default class ViewerDashboard extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <View>
+//           <Text>Some buttons</Text>
+//           <View>
+//             <Button
+//               title="Portal 1"
+//               onPress={() => {
+//                 this.props.navigation.navigate("ViroApp", {
+//                   templateId: 1,
+//                 });
+//               }}
+//             />
+//             <Button
+//               title="Portal 2"
+//               onPress={() => {
+//                 this.props.navigation.navigate("ViroApp", {
+//                   templateId: 2,
+//                 });
+//               }}
+//             />
+//           </View>
+//         </View>
+//       </View>
+//     );
+//   }
+// }
 
 // export default withNavigation(ViewerDashboard);
