5eb7c28985d4b96ad23669ea7c28f0f80f8a3813
Featherweight <vais.emma@gmail.com>
2019-11-27 20:33:42 -0500

    added X button to AR scene - it's not functional yet

--- js/ARPortals/viroApp.js
+++ js/ARPortals/viroApp.js
@@ -1,24 +1,35 @@
-"use strict";
+'use strict';
 
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { ViroARSceneNavigator } from "react-viro";
+import { ViroARSceneNavigator } from 'react-viro';
 
-const initalAR = require("./MainScene");
+import { View, TouchableHighlight, Image } from 'react-native';
+
+import { images } from '../res/images';
+
+const initalAR = require('./MainScene');
 
 export default class ViroApp extends Component {
   render() {
-    console.log(this.props, "props in viroapp js");
+    console.log(this.props, 'props in viroapp js');
     const { navigation } = this.props;
     const portalId = navigation.state.params.portalId;
-    console.log("portalId var in viroApp", portalId);
+    console.log('portalId var in viroApp', portalId);
     return (
-      <ViroARSceneNavigator
-        initialScene={{
-          scene: initalAR,
-          passProps: { portalId: portalId },
-        }}
-      />
+      <View style={{ flex: 3 }}>
+        <ViroARSceneNavigator
+          initialScene={{
+            scene: initalAR,
+            passProps: { portalId: portalId },
+          }}
+        />
+        <View style={{ flex: 1, position: 'absolute' }}>
+          <TouchableHighlight>
+            <Image source={require('../res/icon_close_w.png')} />
+          </TouchableHighlight>
+        </View>
+      </View>
     );
   }
 }
--- js/res/images.js
+++ js/res/images.js
@@ -2,43 +2,43 @@ const images = {
   background: {
     beach: {
       uri:
-        'https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/res/guadalupe_360.jpg',
+        "https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/res/guadalupe_360.jpg",
     },
     party: {
       uri:
-        'https://raw.githubusercontent.com/mARkitFS/mARkit//master/js/res/Kaleidoscope.mp4',
+        "https://raw.githubusercontent.com/mARkitFS/mARkit//master/js/res/Kaleidoscope.mp4",
     },
   },
   element: {
     coolGuy: {
       uri:
-        'https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/res/RUST_3d_Low1.obj',
+        "https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/res/RUST_3d_Low1.obj",
     },
     fox: {
       uri:
-        'https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/low-poly-fox-by-pixelmannen.obj',
+        "https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/low-poly-fox-by-pixelmannen.obj",
       resources: [
-        'https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/low-poly-fox-by-pixelmannen.mtl',
-        'https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/texture.png',
+        "https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/low-poly-fox-by-pixelmannen.mtl",
+        "https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/texture.png",
       ],
     },
     hosted: {
       uri:
-        'https://raw.githubusercontent.com/mARkitFS/mARkit/master/assets/emoji_heart/1180601.glb',
+        "https://raw.githubusercontent.com/mARkitFS/mARkit/master/assets/emoji_heart/1180601.glb",
     },
     heart: {
       uri:
-        'https://raw.githubusercontent.com/mARkitFS/mARkit/master/assets/emoji_heart/emoji_heart.vrx',
+        "https://raw.githubusercontent.com/mARkitFS/mARkit/master/assets/emoji_heart/emoji_heart.vrx",
     },
   },
   thumbnails: {
     PartyEvent: {
       uri:
-        'https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/res/abstract-kaleidoscope-background-beautiful-multicolor-260nw-1084042973.jpg',
+        "https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/res/abstract-kaleidoscope-background-beautiful-multicolor-260nw-1084042973.jpg",
     },
     BeachVacation: {
       uri:
-        'https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/res/guadalupe_360.jpg',
+        "https://raw.githubusercontent.com/mARkitFS/mARkit/master/js/res/guadalupe_360.jpg",
     },
   },
 };
